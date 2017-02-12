c6f6e70f6f578c3e8ac633d4a71220bdcecaec1e
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-02-12 17:58:24 +0530

    typo

--- install_openrc_video.sh
+++ install_openrc_video.sh
@@ -40,7 +40,7 @@ install -Dm755 "${_Iati}" "${DESTDIR}"/etc/init.d/atieventsd
 echo 'ATIEVENTSDOPTS=""' > "${DESTDIR}"/etc/conf.d/atieventsd
 install -Dm644 "${_Cvgl}" "${DESTDIR}"/etc/conf.d/vgl
 install -Dm755 "${_Ivgl}" "${DESTDIR}"/etc/init.d/vgl
-install -Dm755 "${_Svgl}" "${DESTDIR}"/usr/lib${LIBDIRSUFFIX}/${_Svgl}
+install -Dm755 "${_Svgl}" "${DESTDIR}"/usr/lib${LIBDIRSUFFIX}/vgl-helper.sh
 install -d "${DESTDIR}"/var/lib/VirtualGL
 install -Dm644 "${_Cbumb}" "${DESTDIR}"/etc/conf.d/bumblebee
 install -Dm755 "${_Ibumb}" "${DESTDIR}"/etc/init.d/bumblebee
