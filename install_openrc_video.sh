d83774cc54f5ad6fa145a7ef496b4a57c172016c
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-02-12 15:29:44 +0530

    corrected redis and boinc

--- install_openrc_video.sh
+++ install_openrc_video.sh
@@ -7,6 +7,20 @@ DOWNLOAD_DIR=packages-openrc-master
 _local_uri="${DOWNLOAD_DIR}/openrc-video"
 _gentoo_uri="gentoo"
 
+# arch detection for /lib
+if [ -z "$ARCH" ]; then
+  case "$( uname -m )" in
+    i?86) ARCH=i486 ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=$( uname -m ) ;;
+  esac
+fi
+
+LIBDIRSUFFIX=""
+if [ "$ARCH" = "x86_64" ]; then
+  LIBDIRSUFFIX="64"
+fi
+
 # files
 _Cbumb=${_gentoo_uri}/x11-misc/bumblebee/files/bumblebee.confd
 _Ibumb=${_gentoo_uri}/x11-misc/bumblebee/files/bumblebee.initd
@@ -26,7 +40,7 @@ install -Dm755 "${_Iati}" "${DESTDIR}"/etc/init.d/atieventsd
 echo 'ATIEVENTSDOPTS=""' > "${DESTDIR}"/etc/conf.d/atieventsd
 install -Dm644 "${_Cvgl}" "${DESTDIR}"/etc/conf.d/vgl
 install -Dm755 "${_Ivgl}" "${DESTDIR}"/etc/init.d/vgl
-install -Dm755 "${_Svgl}" "${DESTDIR}"/usr/lib/${_Svgl}
+install -Dm755 "${_Svgl}" "${DESTDIR}"/usr/lib${LIBDIRSUFFIX}/${_Svgl}
 install -d "${DESTDIR}"/var/lib/VirtualGL
 install -Dm644 "${_Cbumb}" "${DESTDIR}"/etc/conf.d/bumblebee
 install -Dm755 "${_Ibumb}" "${DESTDIR}"/etc/init.d/bumblebee
