7bb7e9ed23a040bd368c9c2ccaa592ac44d90944
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-05-20 11:49:05 +0530

    minor corrections

--- functions.sh
+++ functions.sh
@@ -4,11 +4,12 @@
 # Called after sourcing the PKGBUILD, such that the source array is declared.
 download_source() {
 	for file in ${source[@]}; do
-		# Check if file is a link or not
-		echo ${file} | grep -q http
-		[ $? -ne 0 ] && return 1
+		# Check if file is a downloadable link or not
+		if ! echo "${file}" | grep -q http; then
+			return 1
+		fi
 		# Get the actual file name
 		filename=${file#*::}
-		wget -c --tries=6 $filename
+		wget -c --tries=4 $filename
 	done
 }
