29f7137a14ff4d5b09f9872e9a775ad3990d80b9
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2018-09-08 13:18:00 +0530

    correct mysqld fixing

--- fix_openrc_devel.sh
+++ fix_openrc_devel.sh
@@ -12,7 +12,7 @@ _p4='s|STOP_TIMEOUT=120|STOP_TIMEOUT=60|'
 sed -e "${_p1}" -i "${DESTDIR}/etc/init.d/mysqld"
 sed -e "${_p2}" -e "${_p3}" -e "${_p4}" -i "${DESTDIR}/etc/conf.d/mysqld"
 # remove extra check
-sed "/\! -d \"\${datadir}\"\/mysql/,+12d" -i "${DESTDIR}/etc/init.d/mysqld"
+sed "/\! -d \"\${datadir}\"\/mysql/,+11d" -i "${DESTDIR}/etc/init.d/mysqld"
 
 # postgresql
 if [ "$(uname -m)" = x86_64 ]; then
