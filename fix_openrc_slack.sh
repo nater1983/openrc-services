4f7a4fdc3391f6afb94208c7695e24f269228fe9
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2018-09-10 01:21:03 +0530

    add fix for dcron not stopping correctly

--- fix_openrc_slack.sh
+++ fix_openrc_slack.sh
@@ -6,7 +6,10 @@ DESTDIR=$1
 # dcron
 # starting slackware current 14.2+
 _p1='/ebegin "Starting \${SVCNAME}"/a \\t# adapted from \/etc\/rc.d\/rc.crond \n\tmkdir -p /run/cron'
-sed -e "${_p1}" -i "${DESTDIR}/etc/init.d/dcron"
+_p2='/start-stop-daemon/,+1d'
+_p3='/ebegin "Stopping \${SVCNAME}"/a \\t# adapted from \/etc\/rc.d\/rc.crond\
+	\/usr\/bin\/pkill --ns \$\$ --euid root -f "^\/usr\/sbin\/crond" 2> \/dev\/null'
+sed -e "${_p1}" -e "${_p2}" -e "${_p3}" -i "${DESTDIR}/etc/init.d/dcron"
 
 # php-fpm
 _p1='s|lib/${PHPSLOT}/bin|sbin|g'
