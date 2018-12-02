1384be4bd1510ab830bb36f710ecd691ab8b905f
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2018-12-02 22:52:57 +0530

    follow SYSCONFDIR for all init and config files

--- fix_path.sh
+++ fix_path.sh
@@ -6,11 +6,13 @@ SYSCONFDIR=${SYSCONFDIR:-/etc}
 
 _shebang='s|#!/sbin/runscript|#!/sbin/openrc-run|'
 _runpath='s|/var/run|/run|g'
+_sysconfdir="s|etc/conf.d|etc/${SYSCONFDIR}|g"
+_sysinitdir="s|etc/init.d|etc/${SYSCONFDIR}|g"
 
 for file in ${DESTDIR}/${SYSCONFDIR}/init.d/*; do
-  sed -e "${_shebang}" -e "${_runpath}" -i "$file"
+  sed -e "${_shebang}" -e "${_runpath}" -e "${_sysconfdir}" -e "${_sysinitdir}" -i "$file"
 done
 
 for file in ${DESTDIR}/${SYSCONFDIR}/conf.d/*; do
-  sed -e "${_runpath}" -i "$file"
+  sed -e "${_runpath}" -e "${_sysconfdir}" -e "${_sysinitdir}" -i "$file"
 done
