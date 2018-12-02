8c5a412355de947754bc6f59d61cf40a51c4b0de
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2018-12-02 23:08:36 +0530

    fix incorrect SYSCONFDIR replacement

--- fix_path.sh
+++ fix_path.sh
@@ -2,12 +2,12 @@
 # fix_path.sh: fixes path in runscript and location of /var/run
 
 DESTDIR=$1
-SYSCONFDIR=${SYSCONFDIR:-/etc}
+SYSCONFDIR=${SYSCONFDIR:-etc}
 
 _shebang='s|#!/sbin/runscript|#!/sbin/openrc-run|'
 _runpath='s|/var/run|/run|g'
-_sysconfdir="s|etc/conf.d|etc/${SYSCONFDIR}|g"
-_sysinitdir="s|etc/init.d|etc/${SYSCONFDIR}|g"
+_sysconfdir="s|etc/conf.d|${SYSCONFDIR}/conf.d|g"
+_sysinitdir="s|etc/init.d|${SYSCONFDIR}/init.d|g"
 
 for file in ${DESTDIR}/${SYSCONFDIR}/init.d/*; do
   sed -e "${_shebang}" -e "${_runpath}" -e "${_sysconfdir}" -e "${_sysinitdir}" -i "$file"
