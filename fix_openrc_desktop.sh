016dbaad7e86ed64b9230bcf924b8d6d79b5bdec
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2019-09-16 11:04:09 +0530

    add todo + fix for updated xdm service

--- fix_openrc_desktop.sh
+++ fix_openrc_desktop.sh
@@ -3,6 +3,7 @@
 
 DESTDIR=$1
 SYSCONFDIR=${SYSCONFDIR:-etc}
+LIBRCDIR=${LIBRCDIR:-usr/libexec/rc}
 
 # bluetooth
 _p1='s|libexec/bluetooth|sbin|g'
@@ -11,8 +12,9 @@ sed -e "${_p1}" -i "${DESTDIR}/${SYSCONFDIR}/init.d/bluetooth"
 # xdm
 _p1='s|/etc/profile.env|/etc/profile|g'
 _p2="s|etc/init.d|${SYSCONFDIR}/init.d|g"
+_p3="s|lib/rc|${LIBRCDIR}|g"
 sed -e "${_p1}" -i "${DESTDIR}/${SYSCONFDIR}/init.d/xdm"
-sed -e "${_p2}" -i "${DESTDIR}/etc/X11/startDM.sh"
+sed -e "${_p2}" -e "${_p3}" -i "${DESTDIR}/etc/X11/startDM.sh"
 
 # wpa_supplicant
 if [ -f /etc/os-release ]; then
